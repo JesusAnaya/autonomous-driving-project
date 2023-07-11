862e96413bed17502cd63b5ee815db2a8306f031
Armando Anaya <jesus@armandoanaya.com>
2023-07-11 02:02:08 -0700

    Updated script to export model from pytorch to onnx using the new normalization.

--- export_model_to_onnx.py
+++ export_model_to_onnx.py
@@ -15,7 +15,7 @@ def main():
     # Create a dummy input that matches the input format of the model
     # The input format of the model is (batch_size, channels, height, width)
     # for this case, width = 200, height = 66, channels = 3
-    dummy_input = torch.randint(0, 255, (1, 3, 66, 200), dtype=torch.float32, device=device) / 255.0
+    dummy_input = torch.randn(1, 3, 66, 200, device=device)
 
     # Export the model to an ONNX file
     torch.onnx.export(
